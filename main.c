4c45b4ffdf56efc73c2958c7d4aad9d904a56813
Msquared <msquared@ubuntu.(none)>
2012-11-25 15:22:52 -0600

    main is coming along

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ int main()
     int time_CPU;
     int time_IO;
     int time_Arrival;
+    /*Job for arrivals*/
+    Job temp_job;
     /*Initialize Job Loader*/
     next_job(-1);
     /*Initialize IO_manager*/
@@ -22,6 +24,23 @@ int main()
         /*time_CPU = next_FinishCPU(clock)*/
         time_IO = next_CompletesIO(clock);
         time_Arrival = next_JobArrival(clock);
+        if(time_CPU <= time_IO && time_CPU <= time_Arrival)
+        {
+            /*Handle context switch*/
+            /*Add to IO if needed*/
+        }
+        if(time_IO <= time_Arrival && time_IO <= time_CPU)
+        {
+            /*Handle finishing IO*/
+        }
+        if(time_Arrival <= time_IO && time_Arrival <= time_CPU)
+        {
+            /*Handle arriving Jobs*/
+            do{
+                temp_job = next_job(clock);
+                /*needs_CPU(temp_job)*/
+            }while(temp_job != NULL);
+        }
     }
     return 0;
 }
