4d8a16dbeb02566739347c665205ecbd78b091f2
Tim Lancina <tim@codiqa.com>
2012-11-25 15:47:07 -0600

    minor edits

--- job.c
+++ job.c
@@ -12,6 +12,8 @@ Job create_job(char * name, int start, double CPU_time, int IO_count)
     strcpy(temp_job->cmd_name,name);
     temp_job->start_time = start;
     temp_job->CPU_time = CPU_time*1000;
+    temp_job->time_remaning = temp_job->CPU_time;
+    temp_job->time_running = 0;
     temp_job->IO_count = IO_count;
     temp_job->IOOperations = trunc ((IO_count + 8191) / 8192);
     temp_job->IO_interval = trunc(temp_job->CPU_time/temp_job->IOOperations);
